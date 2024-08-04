2c1bdd8e968af5dce2e1608385f10bf3fd58dcc5
Nevah <165732847+nevah-evans@users.noreply.github.com>
2024-08-04 11:44:54 -0500

    adding DELETE to category, product, and tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -116,9 +116,22 @@ router.put('/:id', async (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  try {} catch (err) {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({message: 'No product found with this id!'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,7 +52,20 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
-  try {} catch (err) {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!tagData) {
+    res.status(404).json({message: 'No tag found with this id!'});
+    return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
